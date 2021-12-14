f24b4196fb60bf9fe7f0a6c7b7c0643ca62ff423
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2021-12-14 16:05:21 +0000

    Feat: Domain Name Generator function

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your"];
+  let adj = ["great", "brave", "awesome", "clever"];
+  let noun = ["computer", "data", "system", "software"];
+  let dom = [".com", ".es", ".net"];
+  let domIndx = 0;
+
+  pronoun.forEach(pronoun => {
+    adj.forEach(adj => {
+      noun.forEach(noun => {
+        domIndx = Math.floor(Math.random() * dom.length);
+        document.getElementById(
+          "root"
+        ).innerHTML += `${pronoun}${adj}${noun}${dom[domIndx]}<br>`;
+      });
+    });
+  });
 };
